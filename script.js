b0aa011bf128ebfa6c386523ce930b96cf1efac2
FrenzH <franciattom99@gmail.com>
2022-09-05 15:18:29 +0100

    let-5-random-numbers

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log('js ok!')
+const PCnumbersContainer = document.getElementById('PC-numbers-container')
+
+function getRandomNumber(min,max){
+    const range = max - min; 
+    const random = Math.floor(Math.random()*range)  
+    const result = min + random;
+    return result;
+}
+const arr =[]
+while(arr.length<5){
+    let SimonNumb =getRandomNumber(1,50);
+    arr.push(SimonNumb);
+}
+
+
+for (let i=0;i<arr.length;i++){
+    const PCnumbers =document.createElement('div');
+    PCnumbers.innerHTML = arr[i]
+    PCnumbersContainer.append( PCnumbers)
+    PCnumbers.classList.add('PC-numbers')
+    
+
+
+}
+console.log(arr)
